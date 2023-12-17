e993afa51430b720e968dee1132e2d73103b4088
Guy Kaplan <gkpln3@gmail.com>
2023-12-17 16:02:02 +0200

    Fixed mappings and stuff

--- chadrc.lua
+++ chadrc.lua
@@ -2,9 +2,11 @@
 local M = {}
 M.ui = { theme = 'onedark' }
 M.plugins = "custom.plugins"
+M.mappings = require "custom.mappings"
 
 vim.opt.relativenumber = true
 
+-- Configure copy paste to use system clipboard
 local function copy(lines, _)
   require('osc52').copy(table.concat(lines, '\n'))
 end
@@ -19,6 +21,7 @@ vim.g.clipboard = {
   paste = {['+'] = paste, ['*'] = paste},
 }
 
+-- When vim opens, use the directory given as an argument
 vim.api.nvim_create_autocmd("VimEnter", {
     pattern = "*",
     callback = function()
@@ -33,11 +36,10 @@ vim.api.nvim_create_autocmd("VimEnter", {
     end,
 })
 
+-- Make double click select whole words including - and .
 vim.opt.iskeyword:append("-")
 vim.opt.iskeyword:append(".")
-vim.keymap.set("n", "<leader>gs", vim.cmd.Git)
 
-M.mappings = require "custom.mappings"
 vim.g.copilot_assume_mapped = true
 
 -- Disable tab for cmp since it is used for copilot
@@ -55,28 +57,5 @@ if present then
     },
   })
 end
-
- function live_grep_in_nvimtree_directory()
-    local is_nvimtree_open = pcall(vim.api.nvim_buf_get_var, 0, 'nvim_tree')
-
-    if is_nvimtree_open then
-        -- Get the path of the selected item in NvimTree
-        local lib = require('nvim-tree.lib')
-        local node = lib.get_node_at_cursor()
-        local path = node.absolute_path
-
-        -- Check if the path is a directory
-        if node.entries ~= nil then
-            require('telescope.builtin').live_grep({ search_dirs = {path} })
-        else
-            print('Selected item is not a directory')
-        end
-    else
-        print('NvimTree is not focused')
-    end
-end
-
--- Key mapping to invoke the function
-vim.api.nvim_set_keymap('n', '<leader>fw', ':lua live_grep_in_nvimtree_directory()<CR>', { noremap = true, silent = true })
 
 return M
--- mappings.lua
+++ mappings.lua
@@ -1,4 +1,38 @@
 local M = {}
+ function live_grep_in_nvimtree_directory()
+    local nvim_tree_bufnr = vim.fn.bufnr('NvimTree_1')
+    local current_bufnr = vim.api.nvim_get_current_buf()
+    if current_bufnr == nvim_tree_bufnr then
+      -- Get the path of the selected item in NvimTree
+      local lib = require('nvim-tree.lib')
+      local node = lib.get_node_at_cursor()
+      local path = node.absolute_path
+
+      -- Check if the path is a directory
+      if node and node.absolute_path and vim.fn.isdirectory(node.absolute_path) == 1 then
+          require('telescope.builtin').live_grep({ search_dirs = {path} })
+      else
+        require('telescope.builtin').live_grep({ search_dirs = {path} })
+      end
+    else
+      require('telescope.builtin').live_grep({ search_dirs = {path} })
+    end
+end
+
+function live_grep_in_directory()
+    local input_opts = {
+      prompt = 'Enter Directory To Grep In: ',
+      default = '',
+      completion = 'dir',
+    }
+
+    -- Ask for the directory
+    local dir = vim.fn.input(input_opts)
+
+    -- Run Telescope live_grep in the specified directory
+    require('telescope.builtin').live_grep({ search_dirs = {dir} })
+end
+
 M.guy = {
 	n = {
 		["<leader><leader>"] = {"<cmd> Telescope resume <CR><ESC>", "Resume last telescope session"},
@@ -15,6 +49,26 @@ M.guy = {
       end,
       "Goto prev buffer",
     },
+    ["<leader>h"] = {
+      "<cmd>nohlsearch<CR>",
+      "Clear highlights",
+    },
+    ["<leader>ca"] = {
+      "cmd lua vim.lsp.buf.code_action()<CR>",
+      "LSP code action",
+    },
+    ["<leader>gs"] = {
+      "<cmd>Git<CR>",
+      "Git status",
+    },
+    ["<leader>fw"] = {
+      "<cmd>lua live_grep_in_nvimtree_directory()<CR>",
+      "Find word in nvimtree",
+    },
+    ["<leader>fW"] = {
+      "<cmd>lua live_grep_in_directory()<CR>",
+      "Find word in nvimtree",
+    },
 	}
 }
 
@@ -24,8 +78,5 @@ vim.api.nvim_set_keymap('i', '<A-j>', '<Esc>:m .+1<CR>==gi', { noremap = true, s
 vim.api.nvim_set_keymap('i', '<A-k>', '<Esc>:m .-2<CR>==gi', { noremap = true, silent = true })
 vim.api.nvim_set_keymap('v', '<A-j>', ":m '>+1<CR>gv=gv", { noremap = true, silent = true })
 vim.api.nvim_set_keymap('v', '<A-k>', ":m '<-2<CR>gv=gv", { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<leader>q', ':bd<CR>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<leader>ca', '<cmd>lua vim.lsp.buf.code_action()<CR>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<leader>h', ':nohlsearch<CR>', { noremap = true, silent = true })
 
 return M
